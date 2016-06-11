4906a5cff8d8bb28e0e5fcda55da8da9e9dd863a
Samuel Bateman <samuelbateman@outlook.com>
2016-06-11 15:23:59 -0600

    script file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// JSON File Interface
+
 function fetchJSONFile(path, callback) {
    var httpRequest = new XMLHttpRequest();
    httpRequest.onreadystatechange = function() {
@@ -12,12 +15,23 @@ function fetchJSONFile(path, callback) {
    httpRequest.send();
 }
 
+
 // this requests the file and executes a callback with the parsed result once
 //   it is available
-fetchJSONFile('vid1.json', function(data){
-   // do something with your data
+var jsonSampleFileName = 'vid1.json'
+fetchJSONFile(jsonSampleFileName, function(data){
    console.log(data);
 });
+
+
+
+
+
+
+
+
+
+
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
@@ -26,3 +40,54 @@ var playerWidth = windowWidth * 0.6
 var playerHeight = windowHeight * 0.8
 console.log(playerWidth);
 console.log(playerHeight);
+
+//HOLY SHIT DONT LEAVE THIS HERE
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    createPlayer(tempVideoIdenfitifer)
+}
+
+function createPlayer(vidId) {
+    player = new YT.Player('player', {
+        height: playerHeight,
+        width: playerWidth,
+        videoId: vidId,
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+function stopVideo() {
+player.stopVideo();
+}
+
+function playVideo(){
+player.playVideo()
+}
+
+function pauseVideo(){
+player.pauseVideo()
+}
+
+function jumpTo(seconds){
+player.seekTo(seconds, true)
+}
